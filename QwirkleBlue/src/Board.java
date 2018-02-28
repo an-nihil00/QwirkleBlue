938015851efae6532a44dcf0677601659b54b66e
aodeto7604 <aodeto7604@CHHB2ZJKXV1.inst.hcpss.org>
2018-02-28 13:53:26 -0500

    Working tile

--- QwirkleBlue/src/Board.java
+++ QwirkleBlue/src/Board.java
@@ -1,16 +1,17 @@
 import java.util.HashMap;
-
+import java.util.Vector;
 public class Board {
-	HashMap<Integer, Integer> board = new HashMap<Integer, Integer>();
+	HashMap<Integer, Tile> board = new HashMap<Integer, Tile>();
 	Board(){
 		for(int i = 0; i < 10; i++){
-			board.put(i, i+1);
+			
 		}
 	}
-	
+	 
 	public String toString(){
 		return board.toString();
 	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println(new Board());
---
+++ QwirkleBlue/src/Tile.java
@@ -0,0 +1,48 @@
+public class Tile {
+	private String TheColor;
+	private String ThePattern;
+	private int Col;
+	private int Pat;	
+	Tile(int Colour, int Pattern){
+		//Tile made via numbers 0-5 for color and shape respectively
+		Col=Colour;
+		Pat=Pattern;
+		if(Colour==0){
+			TheColor="Orange";
+		}else if(Colour==1){
+			TheColor="Green";
+		}else if(Colour==2){
+			TheColor="Yellow";
+		}else if(Colour==3){
+			TheColor="Red";
+		}else if(Colour==4){
+			TheColor="Blue";
+		}else if(Colour==5){
+			TheColor="Purple";
+		}else{
+			TheColor="Blank";
+		}
+		if(Pattern==0){
+			ThePattern="Cross";
+		}else if(Pattern==1){
+			ThePattern="Clover";
+		}else if(Pattern==2){
+			ThePattern="Star";
+		}else if(Pattern==3){
+			ThePattern="Diamond";
+		}else if(Pattern==4){
+			ThePattern="Square";
+		}else if(Pattern==5){
+			ThePattern="Circle";
+		}else{
+			ThePattern="Blank";
+		}
+	}
+	public int getColour(){
+		return Col;
+	}
+	public int getPattern(){
+		return Pat;
+	}
+	
+}
